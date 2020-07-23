95472b950cb8a94ef806690a1cd463d88e3b4a64
Elmitel Development <>
2020-07-23 14:51:51 +0200

    Initial commit

---
+++ Podfile
@@ -0,0 +1,16 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'testmenu' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for testmenu
+  pod 'SideMenu'
+
+  target 'testmenuTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ testmenu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testmenu
+//
+//  Created by Elmitel Development on 23/07/2020.
+//  Copyright © 2020 Elmitel Development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
